4b7a3440226904361fee4e2a2186c9f19c5c7500
unknown <daksh6928@gmail.com>
2024-01-22 12:10:40 +0530

    second commit

---
+++ 01basics/04_operations.js
@@ -0,0 +1,18 @@
+let str1="Daksh"
+let str2=" patel"
+let str3=str1+str2
+//console.log(str3)
+
+// console.log(2 + "2")
+// console.log(2+3+"2")
+// console.log("2"+2+4)
+
+//console.log(true+2)
+
+let x,y,z
+x=y=z=1
+//console.log(x,y,z)
+
+let gameCounter=100
+console.log(gameCounter++) //post increment
+console.log(++gameCounter) //preincrement
---
+++ 01basics/05_comparision.js
@@ -0,0 +1,14 @@
+console.log(2>3)
+console.log(5<10)
+
+console.log("2">1)
+
+console.log(null==0)
+console.log(null<0)
+console.log(null!=0)
+console.log(null>0)
+console.log(null>=0)
+
+console.log(1=="1")
+
+console.log('1'===1)
